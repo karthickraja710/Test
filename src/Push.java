a89a20d87b7f26bb066d8d3a6f7abea090194447
karthickraja710 <karthickraja3578@gmail.com>
2019-12-16 19:09:30 +0530

    test2 edited

--- src/Push.java
+++ src/Push.java
@@ -1,6 +1,13 @@
 
 public class Push {
-public static void main(String[] args) {
+
+	private void sample2() {
+		System.out.println("welcome");
+
+	}
+	public static void main(String[] args) {
 	System.out.println("hi");
+	
+	
 }
 }
